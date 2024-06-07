c4305706a1c2937cc4b24fcec6e4a9fe277ee4ee
YASH10122 <yashvirani8@gmail.com>
2024-06-07 15:35:08 +0530

    ok

--- calculator/calcu.js
+++ calculator/calcu.js
@@ -5,10 +5,18 @@ Array.from(buttons).forEach((button)=>{
 
     button.addEventListener('click',   (e)=>{
         if(e.target.innerHTML == '='){
-            string = eval(string )
+            string = eval(string);
+            document.querySelector('input').value = string;
         }
-        console.log(e.target)
-        string = string + e.target.innerHTML;
-        document.querySelector('input').value = string;
+        else if(e.target.innerHTML == 'C'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else{
+
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
     })
 })
